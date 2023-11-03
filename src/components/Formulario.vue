20b6f78abd4d0c30a80bcb5b2e7e58ebb205de8a
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2023-11-03 14:42:29 -0300

    feat: aula 01

---
+++ src/components/Formulario.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para a criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>{{ tempoDecorrido }}</strong>
+                    </section>
+                    <button @click="iniciar" class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button @click="finalizar" class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'FormularioSuperior',
+    data(){
+        return{
+            tempoEmSegundos: 0,
+            referenciaCronometro: 0,
+        }
+    },
+    computed: {
+        tempoDecorrido(){
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8);
+        }
+    },
+    methods: {
+        iniciar(){
+            this.referenciaCronometro = setInterval(() => {
+                this.tempoEmSegundos++;
+            },1000)
+        },
+        finalizar(){
+            clearInterval(this.referenciaCronometro);
+        }
+    }
+})
+</script>
+
+<style scoped></style>
